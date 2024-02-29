d80e595489d7549d9a304f8089826946a171f402
X2Borgo <75789972+X2Borgo@users.noreply.github.com>
2024-02-29 10:03:53 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+
+using namespace std;
+
+class attivita
+{
+    private:
+        string nome;
+        string descrizione;
+        int ora_inizio;
+        int ora_fine;
+
+    public:
+
+    string get_nome()
+    {return nome;}
+    void set_nome(string nome_in)
+    {nome = nome_in;}
+
+    string get_descrizione()
+    {return descrizione;}
+    void set_descrizione(string descrizione_in)
+    {descrizione = descrizione_in;}
+
+    int get_ora_inizio()
+    {return ora_inizio;}
+    void set_ora_inizio(int ora_inizio_in)
+    {ora_inizio = ora_inizio_in;}
+
+    int get_ora_fine()
+    {return ora_fine;}
+    void set_ora_fine(int ora_fine_in)
+    {ora_fine = ora_fine_in;})
+
+};
+
+int main()
+{
+    //per grafica usare qt
+
+    
+}
